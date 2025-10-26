19a1d3ed92d02c844dd68b64da29df3359628b79
donaldumoru <donaldumoru101@gmail.com>
2025-10-26 10:31:21 +0100

    refactored to keep code DRY

--- 02. analog-clock/script.js
+++ 02. analog-clock/script.js
@@ -1,70 +1,66 @@
-const clockNumbers = document.querySelectorAll(".number");
-const middleCircle = document.querySelector(".middle-circle");
-const clockNumberSpans = document.querySelectorAll(".number span");
-const secondHand = document.querySelector(".second");
-const minuteHand = document.querySelector(".minute");
-const hourHand = document.querySelector(".hour");
-const hourDisplay = document.querySelector(".hour-display");
-const minuteDisplay = document.querySelector(".minute-display");
-const dayAndDateDisplay = document.querySelector(".day-and-date");
+const clockNumbers = document.querySelectorAll('.number');
+const middleCircle = document.querySelector('.middle-circle');
+const clockNumberSpans = document.querySelectorAll('.number span');
+const secondHand = document.querySelector('.second');
+const minuteHand = document.querySelector('.minute');
+const hourHand = document.querySelector('.hour');
+const hourDisplay = document.querySelector('.hour-display');
+const minuteDisplay = document.querySelector('.minute-display');
+const dayAndDateDisplay = document.querySelector('.day-and-date');
 
-const days = [
-  "sunday",
-  "monday",
-  "tuesday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturday",
-];
+const days = ['sun', 'mon', 'tue', 'wed', 'thur', 'fri', 'sat'];
 
 const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
 ];
 
 const makeTickMarks = function (element) {
   const NUM_TICK_MARKS = 60;
 
   for (let i = 0; i < NUM_TICK_MARKS; i++) {
-    const tickMark = document.createElement("div");
-    tickMark.classList.add("tick-mark");
-
-    element.insertAdjacentElement("afterend", tickMark);
+    const tickMark = document.createElement('div');
+    tickMark.classList.add('tick-mark');
+    element.insertAdjacentElement('afterend', tickMark);
   }
 };
 
 makeTickMarks(middleCircle);
 
-let startDegree = 0;
+const initPositionNumbers = function () {
+  let degree = 0;
 
-const positionClockNumbers = function (numbers, degree) {
-  numbers.forEach((num, index) => {
-    degree += 30;
-    num.style.transform = `rotate(${degree}deg)`;
-    clockNumberSpans[index].style.transform = `rotate(-${degree}deg)`;
-  });
+  const placeItems = (d, op) => `rotate(${op ? op : ''}${d}deg)`;
+
+  return function (numbers) {
+    numbers.forEach((num, index) => {
+      degree += 30;
+      num.style.transform = placeItems(degree);
+      clockNumberSpans[index].style.transform = placeItems(degree, '-');
+    });
+  };
 };
 
-positionClockNumbers(clockNumbers, startDegree);
+const positionClockNumbers = initPositionNumbers();
+positionClockNumbers(clockNumbers);
 
 const positionTickMarks = function (degree) {
-  const ticksMarks = document.querySelectorAll(".tick-mark");
+  const ticksMarks = document.querySelectorAll('.tick-mark');
 
   let currentPosition = 0;
   let updatePosition = 5;
 
-  ticksMarks.forEach((mark) => {
+  ticksMarks.forEach(mark => {
     degree += 6;
 
     mark.style.transform = `rotate(${degree}deg)`;
@@ -72,16 +68,21 @@ const positionTickMarks = function (degree) {
     currentPosition++;
 
     if (currentPosition === updatePosition) {
-      mark.textContent = "❚";
+      mark.textContent = '❚';
       currentPosition = 0;
       return;
     }
 
-    mark.textContent = "❘";
+    mark.textContent = '❘';
   });
 };
 
 positionTickMarks(0);
+
+const updateDigitalDisplay = unit =>
+  unit < 10 ? `${unit}`.padStart(2, '0') : unit;
+
+const rotateTimeHand = degree => `translate(-50%) rotate(${degree}deg)`;
 
 const setupClockUpdate = function () {
   const MAX_SECONDS = 60;
@@ -94,23 +95,21 @@ const setupClockUpdate = function () {
     const hour = dateObj.getHours();
     const minutes = dateObj.getMinutes();
     const seconds = dateObj.getSeconds();
-
     const day = days[dateObj.getDay()];
     const month = months[dateObj.getMonth()];
     const date = dateObj.getDate();
 
-    hourDisplay.textContent = hour < 10 ? `${hour}`.padStart(2, "0") : hour;
-    minuteDisplay.textContent =
-      minutes < 10 ? `${minutes}`.padStart(2, "0") : minutes;
+    hourDisplay.textContent = updateDigitalDisplay(hour);
+    minuteDisplay.textContent = updateDigitalDisplay(minutes);
     dayAndDateDisplay.textContent = `${day}, ${month} ${date}`;
 
     const secondsDegree = (seconds / MAX_SECONDS) * FULL_ROTATION;
     const minutesDegree = (minutes / MAX_MINUTES) * FULL_ROTATION;
     const hourDegree = (hour / MAX_HOURS) * FULL_ROTATION;
 
-    secondHand.style.transform = `translate(-50%) rotate(${secondsDegree}deg)`;
-    minuteHand.style.transform = `translate(-50%) rotate(${minutesDegree}deg)`;
-    hourHand.style.transform = `translate(-50%) rotate(${hourDegree}deg)`;
+    secondHand.style.transform = rotateTimeHand(secondsDegree);
+    minuteHand.style.transform = rotateTimeHand(minutesDegree);
+    hourHand.style.transform = rotateTimeHand(hourDegree);
   };
 };
 
